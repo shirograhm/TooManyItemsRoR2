fcb71f707cba1c154d568cf757ef439f692591ae
Xavier Graham <xavierwg13@gmail.com>
2024-06-20 15:42:08 -0700

    refactor

--- TooManyItems/Utils/AssetHandler.cs
+++ TooManyItems/Utils/AssetHandler.cs
@@ -1,12 +1,16 @@
-﻿using System.IO;
+﻿using R2API;
+using System.IO;
 using UnityEngine;
 
 namespace TooManyItems
 {
-    public static class Assets
+    public static class AssetHandler
     {
         public static AssetBundle bundle;
         public const string bundleName = "tmiassets";
+
+        public const uint LUNAR_REVIVE_TICKING_ID = 954093529;
+        public const uint TROWEL_CONSUME_ID = 522946673;
 
         public static string AssetBundlePath
         {
@@ -19,6 +23,8 @@ namespace TooManyItems
         public static void Init()
         {
             bundle = AssetBundle.LoadFromFile(AssetBundlePath);
+            // Load sounds
+            SoundAPI.SoundBanks.Add(File.ReadAllBytes(Path.Combine(Path.GetDirectoryName(TooManyItems.PInfo.Location), "TMI_SoundBank.bnk")));
         }
     }
 }
