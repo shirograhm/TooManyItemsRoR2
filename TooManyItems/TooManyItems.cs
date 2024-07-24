c3a4ceb51ed860ae842067a00420c93aa6c420a5
Xavier Graham <xavierwg13@gmail.com>
2024-07-23 22:21:37 -0700

    Update TooManyItems.cs

--- TooManyItems/TooManyItems.cs
+++ TooManyItems/TooManyItems.cs
@@ -26,7 +26,7 @@ namespace TooManyItems
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "shirograhm";
         public const string PluginName = "TooManyItems";
-        public const string PluginVersion = "0.4.4";
+        public const string PluginVersion = "0.4.6";
 
         public static PluginInfo PInfo { get; private set; }
 
