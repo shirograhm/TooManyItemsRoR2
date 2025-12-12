1941a8fb7cde60242cc44e77b11bec1c5644eded
shirograhm <xavierwg13@gmail.com>
2025-12-12 13:52:47 -0800

    Update TooManyItems.cs
    version

--- TooManyItems/TooManyItems.cs
+++ TooManyItems/TooManyItems.cs
@@ -25,7 +25,7 @@ namespace TooManyItems
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "shirograhm";
         public const string PluginName = "TooManyItems";
-        public const string PluginVersion = "0.6.5";
+        public const string PluginVersion = "0.6.6";
 
         public static PluginInfo PInfo { get; private set; }
 
@@ -201,13 +201,12 @@ namespace TooManyItems
 
         //    if (Input.GetKeyDown(KeyCode.F2))
         //    {
-        //        DropItem(Epinephrine.itemDef);
         //        DropItem(DoubleDown.itemDef);
-        //        DropItem(Crucifix.itemDef);
-        //        DropItem(RustyTrowel.itemDef);
-        //        DropItem(SoulRing.itemDef);
-        //        DropItem(DLC2Content.Items.DelayedDamage);
-        //        DropItem(Vanity.equipmentDef);
+        //        DropItem(BloodDice.itemDef);
+        //        DropItem(GlassMarbles.itemDef);
+        //        DropItem(BrokenMask.itemDef);
+        //        DropItem(Photodiode.itemDef);
+        //        DropItem(HereticSeal.itemDef);
         //    }
         //    if (Input.GetKeyDown(KeyCode.F3))
         //    {
@@ -220,7 +219,7 @@ namespace TooManyItems
         //                {
         //                    victimObject = body.gameObject,
         //                    attackerObject = body.gameObject,
-        //                    totalDamage = body.healthComponent.fullCombinedHealth * 0.2f,
+        //                    totalDamage = 10f,
         //                    dotIndex = DotController.DotIndex.Burn,
         //                    duration = 0f,
         //                    damageMultiplier = 1f
