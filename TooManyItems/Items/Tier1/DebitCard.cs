a304204d253fd743321f7c65a15ecbba023d7bb3
shirograhm <xavierwg13@gmail.com>
2025-12-13 14:26:48 -0800

    Logbook Item Displays White

--- TooManyItems/Items/Tier1/DebitCard.cs
+++ TooManyItems/Items/Tier1/DebitCard.cs
@@ -52,8 +52,15 @@ namespace TooManyItems
 
             Utils.SetItemTier(itemDef, ItemTier.Tier1);
 
+            GameObject prefab = AssetHandler.bundle.LoadAsset<GameObject>("DebitCard.prefab");
+            ModelPanelParameters modelPanelParameters = prefab.AddComponent<ModelPanelParameters>();
+            modelPanelParameters.focusPointTransform = prefab.transform;
+            modelPanelParameters.cameraPositionTransform = prefab.transform;
+            modelPanelParameters.maxDistance = 10f;
+            modelPanelParameters.minDistance = 5f;
+
             itemDef.pickupIconSprite = AssetHandler.bundle.LoadAsset<Sprite>("DebitCard.png");
-            itemDef.pickupModelPrefab = AssetHandler.bundle.LoadAsset<GameObject>("DebitCard.prefab");
+            itemDef.pickupModelPrefab = prefab;
             itemDef.canRemove = true;
             itemDef.hidden = false;
 
--- TooManyItems/Items/Tier1/RedBlueGlasses.cs
+++ TooManyItems/Items/Tier1/RedBlueGlasses.cs
@@ -62,8 +62,15 @@ namespace TooManyItems
 
             Utils.SetItemTier(itemDef, ItemTier.Tier1);
 
+            GameObject prefab = AssetHandler.bundle.LoadAsset<GameObject>("3DGlasses.prefab");
+            ModelPanelParameters modelPanelParameters = prefab.AddComponent<ModelPanelParameters>();
+            modelPanelParameters.focusPointTransform = prefab.transform;
+            modelPanelParameters.cameraPositionTransform = prefab.transform;
+            modelPanelParameters.maxDistance = 10f;
+            modelPanelParameters.minDistance = 5f;
+
             itemDef.pickupIconSprite = AssetHandler.bundle.LoadAsset<Sprite>("3DGlasses.png");
-            itemDef.pickupModelPrefab = AssetHandler.bundle.LoadAsset<GameObject>("3DGlasses.prefab");
+            itemDef.pickupModelPrefab = prefab;
             itemDef.canRemove = true;
             itemDef.hidden = false;
 
