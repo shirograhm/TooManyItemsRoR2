2c07660df9d60a886dbcd925715e05961a2d3a0d
shirograhm <xavierwg13@gmail.com>
2025-12-13 16:08:07 -0800

    Update TooManyItems.cs
    version number

--- TooManyItems/TooManyItems.cs
+++ TooManyItems/TooManyItems.cs
@@ -25,7 +25,7 @@ namespace TooManyItems
         public const string PluginGUID = PluginAuthor + "." + PluginName;
         public const string PluginAuthor = "shirograhm";
         public const string PluginName = "TooManyItems";
-        public const string PluginVersion = "0.6.7";
+        public const string PluginVersion = "0.6.8";
 
         public static PluginInfo PInfo { get; private set; }
 
