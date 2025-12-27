5441d92c3d467e208bf3aacc4dc93d470e86cd25
shirograhm <xavierwg13@gmail.com>
2025-12-26 17:49:17 -0800

    Balance

--- TooManyItems/Items/Tier2/Epinephrine.cs
+++ TooManyItems/Items/Tier2/Epinephrine.cs
@@ -28,7 +28,7 @@ namespace TooManyItems.Items.Tier2
         public static ConfigurableValue<float> buffDuration = new(
             "Item: Epinephrine",
             "Buff Duration",
-            1f,
+            1.5f,
             "Duration of attack speed gained after taking damage.",
             ["ITEM_EPINEPHRINE_DESC"]
         );
