a2f304ecde60e3c61bf342d3a6dce87f1455cab4
shirograhm <xavierwg13@gmail.com>
2025-12-31 15:00:52 -0800

    Update Crucifix.cs
    balance

--- TooManyItems/Items/Lunar/Crucifix.cs
+++ TooManyItems/Items/Lunar/Crucifix.cs
@@ -32,7 +32,7 @@ namespace TooManyItems.Items.Lunar
         public static ConfigurableValue<float> maxHealthBurnAmountReduction = new(
             "Item: Crucifix",
             "Burn Amount Reduction",
-            25f,
+            15f,
             "Percentage of burn damage reduced per stack of this item. This scales hyperbolically.",
             ["ITEM_CRUCIFIX_DESC"]
         );
